f17fecc972b1350df7e08cd81914435cf670d80b
Cleverfox5 <olshanov03@mail.ru>
2026-03-16 01:50:20 +0300

    cmake : add libsodium

---
+++ first_server_src/PhotogramAuth.cpp
@@ -0,0 +1,21 @@
+﻿//#include "PhotogramAuth.h"
+//#include "MyServer.h"
+
+#include <nlohmann/json.hpp>
+#include <jwt-cpp/jwt.h>
+#include <pqxx/pqxx>
+#include <sodium.h>
+
+
+const short PORT = 8080;
+
+
+int main(int argc, char* argv[]) {
+    //MyServer server(PORT);
+    //server.run();
+    getchar();
+    std::cout << "The server is shutting down\n";
+    //server.stop();
+
+    return 0;
+}
