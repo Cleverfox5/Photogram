4e1f8c9ac2d6e827869f4311ea39b6c90175c902
Cleverfox5 <olshanov03@mail.ru>
2026-05-02 02:53:50 +0300

    main : Added creation of the jwt token

--- second_server_src/ClientAndServer.cpp
+++ second_server_src/ClientAndServer.cpp
@@ -3,12 +3,23 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include "TcpServer.hpp"
+#include <jwt-cpp/jwt.h>
+#include <chrono>
 
 using boost::asio::ip::tcp;
 namespace http = boost::beast::http;
 
 int main()
 {
+    auto token = jwt::create()
+        .set_type("JWS")
+        .set_issuer("PhotogramAuth")
+        .set_subject("1")
+        .set_issued_at(std::chrono::system_clock::now())
+        .set_expires_at(std::chrono::system_clock::now() + std::chrono::hours(7))
+        .sign(jwt::algorithm::hs256{ "1312123" });
+
+
     try
     {
         boost::asio::io_context io_context_client;
